98fa14ab73b8bd09197d700f816361a7a198b0ac
DevGabiMrqs <gabriellimrqs1998@gmail.com>
2024-04-01 22:15:05 -0300

    final-updates

--- src/lib.ts
+++ src/lib.ts
@@ -2,15 +2,33 @@ class GeoLib {
   public getAddressFromCoordinates(
     coordinates: [number, number] | { lat: number; lng: number },
   ): Promise<string> {
-    return Promise.resolve("Rua teste");
-    // return Promise.reject(new Error("Not implemented"));
+    const address = this.convertCoordinatesToAddress(coordinates);
+    return Promise.resolve(address);
   }
 
   public getCoordinatesFromAddress(
     address: string,
   ): Promise<{ lat: number; lng: number }> {
-    return Promise.resolve({ lat: 1651, lng: 5416 });
-    // return Promise.reject(new Error("Not implemented"));
+    const coordinates = this.convertAddressToCoordinates(address);
+    return Promise.resolve(coordinates);
+  }
+
+  private convertCoordinatesToAddress(
+    coordinates: [number, number] | { lat: number; lng: number },
+  ): string {
+    if (Array.isArray(coordinates)) {
+      return `Latitude: ${coordinates[0]}, Longitude: ${coordinates[1]}`;
+    } else {
+      return `Latitude: ${coordinates.lat}, Longitude: ${coordinates.lng}`;
+    }
+  }
+
+  private convertAddressToCoordinates(address: string): {
+    lat: number;
+    lng: number;
+  } {
+    // Simulando uma conversão fictícia de endereço para coordenadas
+    return { lat: 37.7749, lng: -122.4194 };
   }
 }
 
